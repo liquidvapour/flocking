236eeeb486ae37960962954446bd41f011eb022a
Ra-el Peters <ra-el@gajatech.org>
2025-02-16 13:57:56 +0000

    tweaks to food

--- food.js
+++ food.js
@@ -3,7 +3,7 @@ const FOOD_AMOUNT = 1000;
 export class Food {
   constructor(scene) {
     const foodGeometry = new THREE.SphereGeometry(3, 16, 16);
-    const foodMaterial = new THREE.MeshLambertMaterial({ color: 0xffaa00 });
+    const foodMaterial = new THREE.MeshLambertMaterial({ color: 0xff00aa });
     this.mesh = new THREE.Mesh(foodGeometry, foodMaterial);
     this.mesh.position.set((Math.random() - 0.5) * 200, 3, (Math.random() - 0.5) * 200);
     scene.add(this.mesh);
